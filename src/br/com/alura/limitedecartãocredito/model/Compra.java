834ed4d72beae7e30f602fe108183e7bc37e4da1
Deivaaaaaaa <deivysonrssantaaaaaaaa>
2025-06-30 12:20:55 -0300

    Retormando os Estudos JAVA

---
+++ src/br/com/alura/limitedecartãocredito/model/Compra.java
@@ -0,0 +1,26 @@
+package br.com.alura.limitedecartãocredito.model;
+
+public class Compra {
+    private String  descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras Realizadas\n: " +
+                "descricao='" + descricao + '\'' +
+                "valor=" + valor;
+    }
+}
