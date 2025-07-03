532ce8d7ca4554227a5c46da01db171c4d6efdca
Gio-Midonz-Bots <roboticagiovanna@gmail.com>
2025-07-03 14:48:30 -0300

    ADD: Gerar Arquivo json

--- src/br/com/POOalura/ViaCep/Principal.java
+++ src/br/com/POOalura/ViaCep/Principal.java
@@ -1,10 +1,23 @@
 package br.com.POOalura.ViaCep;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         ConsultaCep colsuta = new ConsultaCep();
-        Endereco novo = colsuta.buscarEndereco("01001000");
+
+        System.out.println("Digite um número de CEP: ");
+        String CEP = sc.nextLine();
+        try{
+        Endereco novo = colsuta.buscarEndereco(CEP);
         System.out.println(novo);
+        GeradorDeArquivo gerador = new GeradorDeArquivo();
+        gerador.gerarArquivo(novo);
+        }catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizado a aplicação");
+        }
     }
 }
